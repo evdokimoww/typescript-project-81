c0cd284ce44711b028af01234da76a182e9f5562
aevdokimov <aevdokimov@t1-group.ru>
2025-07-02 13:31:40 +0300

    task 2: class tag

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello TS')
+class Tag {
+    private pairedTags: string[] = ['label', 'div']
+
+    constructor(public tagName: string,
+                public options: {[key: string]: string} = {},
+                public inner: string = '') {}
+
+    toString(): string {
+        const optionsStr = Object.entries(this.options).reduce((acc, [key, value]) => {
+            const str = `${key}="${value}"`
+            return acc ? `${acc} ${str}` : ` ${str}`
+        }, '');
+
+        const startTag = `<${this.tagName}${optionsStr.length ? optionsStr : ''}>`
+
+        if (this.pairedTags.includes(this.tagName)) {
+            return `${startTag}${this.inner}</${this.tagName}>`
+        }
+
+        return startTag
+    }
+}
